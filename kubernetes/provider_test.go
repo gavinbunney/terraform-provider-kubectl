89256636d0a2d875a1ffacbd747fbdbcbca2812e
Gavin Bunney <gavin.bunney@gmail.com>
2019-08-26 08:14:03 -0700

    go fmt

--- kubernetes/provider_test.go
+++ kubernetes/provider_test.go
@@ -16,7 +16,7 @@ var testAccProvider *schema.Provider
 func init() {
 	testAccProvider = Provider().(*schema.Provider)
 	testAccProviders = map[string]terraform.ResourceProvider{
-		"kubectl":     testAccProvider,
+		"kubectl":    testAccProvider,
 		"kubernetes": upstream.Provider(),
 	}
 }
