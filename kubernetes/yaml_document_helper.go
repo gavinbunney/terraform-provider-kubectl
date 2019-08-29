69ec35d1e23e2508d4d8caba578d0d795e9597d4
Gavin Bunney <gavin.bunney@gmail.com>
2019-08-29 08:06:05 -0700

    Refactor YAML document splitting

---
+++ kubernetes/yaml_document_helper.go
@@ -0,0 +1,69 @@
+package kubernetes
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	yamlParser "gopkg.in/yaml.v2"
+	"strings"
+)
+
+func splitMultiDocumentYAML(multidoc string) (documents []string, err error) {
+	content := []byte(multidoc)
+
+	scanner := bufio.NewScanner(bytes.NewReader(content))
+	scanner.Split(splitYAMLDocument)
+
+	for scanner.Scan() {
+		document := strings.TrimSpace(scanner.Text())
+
+		// attempt to parse the document as yaml
+		rawYamlParsed := &map[string]interface{}{}
+		err := yamlParser.Unmarshal([]byte(document), rawYamlParsed)
+		if err != nil {
+			return documents, fmt.Errorf("Error parsing yaml document: %v\n%v", err, document)
+		}
+
+		// skip empty yaml documents
+		if len(*rawYamlParsed) == 0 {
+			continue
+		}
+
+		// save to our collection of docs
+		documents = append(documents, document)
+	}
+
+	return documents, nil
+}
+
+const yamlSeparator = "\n---"
+
+// splitYAMLDocument is a bufio.SplitFunc for splitting YAML streams into individual documents.
+func splitYAMLDocument(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	if atEOF && len(data) == 0 {
+		return 0, nil, nil
+	}
+	sep := len([]byte(yamlSeparator))
+	if i := bytes.Index(data, []byte(yamlSeparator)); i >= 0 {
+		// We have a potential document terminator
+		i += sep
+		after := data[i:]
+		if len(after) == 0 {
+			// we can't read any more characters
+			if atEOF {
+				return len(data), data[:len(data)-sep], nil
+			}
+			return 0, nil, nil
+		}
+		if j := bytes.IndexByte(after, '\n'); j >= 0 {
+			return i + j + 1, data[0 : i-sep], nil
+		}
+		return 0, nil, nil
+	}
+	// If we're at EOF, we have a final, non-terminated line. Return it.
+	if atEOF {
+		return len(data), data, nil
+	}
+	// Request more data.
+	return 0, nil, nil
+}
---
+++ kubernetes/yaml_document_helper_test.go
@@ -0,0 +1,73 @@
+package kubernetes
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestYAMLDocumentHelper(t *testing.T) {
+	testCases := []struct {
+		description  string
+		yaml         string
+		expectedDocs []string
+	}{
+		{
+			description: "Test single document",
+			yaml: buildTestData(1),
+			expectedDocs: []string{"kind: Service1"},
+		},
+		{
+			description: "Test multi document",
+			yaml: buildTestData(2),
+			expectedDocs: []string{"kind: Service1", "kind: Service2"},
+		},
+		{
+			description: "Test multi document with empty document at end",
+			yaml: buildTestData(2) + `
+---
+# just a comment
+---
+`,
+			expectedDocs: []string{"kind: Service1", "kind: Service2"},
+		},
+		{
+			description: "Test multi document with empty document at start",
+			yaml: `
+---
+# just a comment
+---
+` + buildTestData(2),
+			expectedDocs: []string{"kind: Service1", "kind: Service2"},
+		},
+		{
+			description: "Test multi document with only empty documents",
+			yaml: `
+---
+# just a comment
+---
+# more empty docs
+---
+`,
+			expectedDocs: nil,
+		},
+	}
+
+	for _, tcase := range testCases {
+		t.Run(tcase.description, func(t *testing.T) {
+			result, err := splitMultiDocumentYAML(tcase.yaml)
+			assert.NoError(t, err, "Expect to succeed")
+			assert.Equal(t, len(tcase.expectedDocs), len(result), "Expect docs count to match")
+			assert.Equal(t, tcase.expectedDocs, result, "Expect docs to match")
+		})
+	}
+}
+
+func buildTestData(count int) (content string) {
+	for i := 1; i <= count; i++ {
+		content += fmt.Sprintf("\nkind: Service%v\n---", i)
+	}
+
+	return content
+}
