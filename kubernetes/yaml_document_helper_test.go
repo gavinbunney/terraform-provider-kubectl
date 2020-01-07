9e711d342fafb55b990e464044f088f2e3c525d5
Yilong Shu <yshu@guidewire.com>
2020-01-07 11:45:06 -0600

    GWRE-100: move test file to _examples directory

--- kubernetes/yaml_document_helper_test.go
+++ kubernetes/yaml_document_helper_test.go
@@ -98,7 +98,7 @@ func buildTestData(count int) (content string) {
 
 func readTestFile() (content string) {
 
-	path := "./resources/01-cert-manager-crds.yaml"
+	path := "../_examples/cert-manager/01-cert-manager-crds.yaml"
 	file, err := ioutil.ReadFile(path)
 	check(err)
 
