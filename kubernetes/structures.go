d8a2ac805c6abf83b73ce593577221082936c22b
rinrailin <trifonov.anton@pdffiller.team>
2020-01-23 14:45:33 +0200

    Add missed expandStringSlice function

---
+++ kubernetes/structures.go
@@ -0,0 +1,14 @@
+package kubernetes
+
+func expandStringSlice(s []interface{}) []string {
+	result := make([]string, len(s), len(s))
+	for k, v := range s {
+		// Handle the Terraform parser bug which turns empty strings in lists to nil.
+		if v == nil {
+			result[k] = ""
+		} else {
+			result[k] = v.(string)
+		}
+	}
+	return result
+}
