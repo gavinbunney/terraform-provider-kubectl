c84e8eab362e10e6bc9c00050de5d27413b86df7
Gavin Bunney <409207+gavinbunney@users.noreply.github.com>
2020-05-04 16:02:59 -0700

    Bump k3s on CI

--- scripts/startk3s_ci.sh
+++ scripts/startk3s_ci.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "--> Downloading K3s"
-curl -Lo k3s https://github.com/rancher/k3s/releases/download/v0.3.0/k3s && chmod +x k3s
+curl -Lo k3s https://github.com/rancher/k3s/releases/download/v1.0.1/k3s && chmod +x k3s
 
 echo "--> Starting K3s"
 sudo ./k3s server &
