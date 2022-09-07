9a1755ea625b4d205ae4d0aa8cc0ecc1c598e433
Michiel Boekhoff <michielalexanderb@gmail.com>
2022-09-07 11:02:25 +0100

    Update terraform-plugin-sdk.
    This fixes the acceptance tests, as this also pulls in a new version of
    terraform-json. See also
    https://github.com/hashicorp/terraform-plugin-sdk/issues/821 for more
    details.

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	kubernetes "github.com/gavinbunney/terraform-provider-kubectl/kubernetes"
 	goplugin "github.com/hashicorp/go-plugin"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
-	tf5server "github.com/hashicorp/terraform-plugin-go/tfprotov5/server"
+	tf5server "github.com/hashicorp/terraform-plugin-go/tfprotov5/tf5server"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"google.golang.org/grpc"
