4703a1fc202ffa06b27525d8effd9ff7be7a401f
Gavin Bunney <gbunney@netflix.com>
2024-11-29 19:13:25 -0800

    Expand testing of k8s versions

--- scripts/start-k3s.sh
+++ scripts/start-k3s.sh
@@ -9,6 +9,8 @@ export COMPOSE_PROJECT_NAME=k3s
 export ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 export DOCKER_DEFAULT_PLATFORM=linux/${ARCH}
 
+export K3S_VERSION=${K3S_VERSION:-v1.31.2-k3s1}
+
 echo "--> Tearing down k3s in docker-compose"
 docker-compose down -v &>/dev/null || true
 rm -rf ${KUBECONFIG}
--- scripts/stop-k3s.sh
+++ scripts/stop-k3s.sh
@@ -8,6 +8,8 @@ export COMPOSE_PROJECT_NAME=k3s
 export ARCH=$(uname -m | tr '[:upper:]' '[:lower:]')
 export DOCKER_DEFAULT_PLATFORM=linux/${ARCH}
 
+export K3S_VERSION=${K3S_VERSION:-v1.31.2-k3s1}
+
 echo "--> Stopping k3s in docker-compose"
 docker-compose down -v
 rm -rf kubeconfig.yaml
