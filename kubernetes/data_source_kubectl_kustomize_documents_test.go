c9dc419d619ef72641f7df0cd8124f8b2f2b12b1
Gavin Bunney <409207+gavinbunney@users.noreply.github.com>
2024-12-09 13:50:20 -0800

    Expand kustomize test coverage and update for later kustomize format (#328)

--- kubernetes/data_source_kubectl_kustomize_documents_test.go
+++ kubernetes/data_source_kubectl_kustomize_documents_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-var kustTargetUrl = "https://github.com/kubernetes-sigs/kustomize/examples/multibases?ref=v1.0.6"
+var kustTargetUrl = "https://github.com/kubernetes-sigs/kustomize/examples/multibases?ref=v3.3.1"
 
 func TestAccKubectlDataSourceKustomizeDocuments_url(t *testing.T) {
 	resource.Test(t, resource.TestCase{
@@ -28,7 +28,73 @@ func TestAccKubectlDataSourceKustomizeDocuments_localDir(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: kubectlKustomizeDocumentsConfig("../test/data/kustomize/helloWorld"),
-				Check:  resource.TestCheckResourceAttr("data.kubectl_kustomize_documents.test", "documents.#", "3"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.kubectl_kustomize_documents.test", "documents.#", "3"),
+					resource.TestCheckResourceAttr("data.kubectl_kustomize_documents.test", "documents.0", `apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: hello
+  name: the-deployment
+spec:
+  replicas: 3
+  selector:
+    matchLabels:
+      app: hello
+  template:
+    metadata:
+      labels:
+        app: hello
+        deployment: hello
+    spec:
+      containers:
+      - command:
+        - /hello
+        - --port=8080
+        - --enableRiskyFeature=$(ENABLE_RISKY)
+        env:
+        - name: ALT_GREETING
+          valueFrom:
+            configMapKeyRef:
+              key: altGreeting
+              name: the-map
+        - name: ENABLE_RISKY
+          valueFrom:
+            configMapKeyRef:
+              key: enableRisky
+              name: the-map
+        image: monopole/hello:1
+        name: the-container
+        ports:
+        - containerPort: 8080
+`),
+					resource.TestCheckResourceAttr("data.kubectl_kustomize_documents.test", "documents.1", `apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: hello
+  name: the-service
+spec:
+  ports:
+  - port: 8666
+    protocol: TCP
+    targetPort: 8080
+  selector:
+    app: hello
+    deployment: hello
+  type: LoadBalancer
+`),
+					resource.TestCheckResourceAttr("data.kubectl_kustomize_documents.test", "documents.2", `apiVersion: v1
+data:
+  altGreeting: Good Morning!
+  enableRisky: "false"
+kind: ConfigMap
+metadata:
+  labels:
+    app: hello
+  name: the-map
+`),
+				),
 			},
 		},
 	})
